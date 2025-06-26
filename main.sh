1f459eafcea9dfe78fd44fd213c933d765ce468a
Anton1o Vakhnin <anton1ovakhnin@MacBook-Air-Anton1o.local>
2025-06-26 18:45:29 +0300

    Task 2-Creating files based on the main one with filtering by status

--- main.sh
+++ main.sh
@@ -1,15 +1,27 @@
 #!bin/bash
 
 LINK=https://raw.githubusercontent.com/GreatMedivack/files/master/list.out
+FILE=test.out
 
-TEST=test.out
+# 
+ALPHABET='[^a-zA-Z]+'
+NUMBER='[^0-9]+'
+
+DATE=$(date +"%d_%m_%Y")
+
+# параметр
+SERVER=$1
+
+RUNNING_FILE=${SERVER}_${DATE}_running.out
+FAILED_FILE=${SERVER}_${DATE}_failed.out
+
+
 
 # Проверка формата url
 if [[ ! "$LINK" =~ ^https:// ]]; then 
 	echo "Ошибка: Не соответсвует формату" >&2
 	exit 1
 fi
-
 
 # Проверка доступности url
 if ! curl --output /dev/null --silent --head --fail "$LINK"; then
@@ -18,16 +30,55 @@ if ! curl --output /dev/null --silent --head --fail "$LINK"; then
 fi
 
 # загрузка данных и сохр. в файл
-if curl --silent --fail "$LINK" --output "$TEST"; then
-	if [[ -s "$TEST" ]]; then
+if curl --silent --fail "$LINK" --output "$FILE"; then
+	if [[ -s "$FILE" ]]; then
 		echo "Данные сохр в "$TEST""
-		echo "Размер файла: $(wc -l < "$TEST") строк"
+		echo "Размер файла: $(wc -l < "$FILE") строк"
 	else
 		echo "Ошибка: файл пустой" >&2
-		rm -rf "$TEST"
+		rm -rf "$FILE"
 		exit 1
 	fi
 else
 	echo "Ошибка при загрузке данных" >&2
 	exit 1
 fi 	
+
+# Распределение по файлам, + удаление постфиксов 
+awk -v failed_file="$FAILED_FILE" -v running_file="$RUNNING_FILE" '
+NR > 1 {  # Пропускаем заголовок
+    # Удаляем все постфиксы вида -xxxxx (включая цифры и буквы разной длины)
+    service_name = $1
+    sub(/-[^-]+(-[^-]+)?$/, "", service_name)
+    
+    if ($3 == "Error" || $3 == "CrashLoopBackOff") {
+        print service_name >> failed_file
+    } 
+    else if ($3 == "Running") {
+        print service_name >> running_file
+    }
+}' "$FILE"
+
+
+# Проверка на созд файлы и они не пусты
+if [[ !  -s "$RUNNING_FILE" ]]; then
+	echo "Предупреждение: нету запущенных сервисов"
+	touch $RUNNING_FILE
+fi
+
+if [[ !  -s "$FAILED_FILE" ]]; then
+	echo "Предупреждение: нету неработающих сервисов"
+	touch $FAILED_FILE
+fi 
+
+\
+echo "Готово! Результаты сохранены в:"
+echo "- $FAILED_FILE ($(wc -l < "$FAILED_FILE") сервисов с ошибками)"
+echo "- $RUNNING_FILE ($(wc -l < "$RUNNING_FILE") работающих сервисов)"
+
+
+
+
+
+
+
